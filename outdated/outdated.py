4c5de77afc36ef3255cbee5f224f991ac446919e
jameskiongo <kiongojames2000@gmail.com>
2023-11-08 14:14:14 +0300

    outdated assignment

---
+++ outdated/outdated.py
@@ -0,0 +1,16 @@
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+#prompt user for date
+#must be month-day-year order
