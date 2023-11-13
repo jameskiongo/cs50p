5787294473214e5f5b006f6b6847fec6df29bafb
jameskiongo <kiongojames2000@gmail.com>
2023-11-13 21:03:19 +0300

    latest lesson

--- libraries/name.py
+++ libraries/name.py
@@ -2,7 +2,6 @@ import sys
 
 if len(sys.argv) < 2:
     sys.exit("too few arguments")
-elif len(sys.argv) > 2:
-    sys.exit("too many arguments")
-else:
-    print(f"hello, my name is {sys.argv[1]}")
+
+for arg in sys.argv[1:]:
+    print("hello, my name is", arg)
