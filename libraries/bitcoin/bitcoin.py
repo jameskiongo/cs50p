c4c421bdab8e6a624080efde4d331b2ef49cd0fa
jameskiongo <kiongojames2000@gmail.com>
2023-11-15 21:30:26 +0300

    completed bitcoin assignment

--- libraries/bitcoin/bitcoin.py
+++ libraries/bitcoin/bitcoin.py
@@ -2,14 +2,27 @@ import json
 import requests
 import sys
 # cli must be there
-# if len(sys.argv) != 2:
-#     sys.exit("Missing command-line argument")
+if len(sys.argv) != 2:
+    sys.exit("Missing command-line argument")
+while True:
+    try:
+        number = float(sys.argv[1])
+        break
+    except:
+        sys.exit("Command-line argument is not a number")
 
-response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+while True:
+    try:
+        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+        o = response.json()
+        bitcoin = o["bpi"]["USD"]["rate_float"]
+        total = number * bitcoin
+        print(f"${total:,.4f}")
+        break
+    except requests.RequestException:
+        ...
+    
 
-o = response.json()
-for result in o["bpi"]:
-    print(result[1][2])
 # print(response)
 # be in float
 # output bitcoin in usd using , as a thousands separator
