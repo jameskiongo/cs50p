c92666907ac943c5855b069866176bc0304e18ee
james kiongo <jameskiongo@jamess-MacBook-Air.local>
2023-11-21 13:39:45 +0300

    finished professor

--- libraries/profesor/professor.py
+++ libraries/profesor/professor.py
@@ -3,39 +3,58 @@ import sys
 
 
 def main():
-    for time in range(2):
-        generate_integer(get_level())
-        calc = x + y
-    # print(f"{x} + {y} = ", end = "")
-    # ans = int(input(""))
-    # if ans == calc:
-    #     break
-    # else:
-    #     print("EEE")
-    #     pass
-
+    level = get_level()
+    score = simulate_game(level)
+    print("Score: ", score)
 
 def get_level():
     while True:
         try:
-            n = int(input("Level: "))
-            if n < 1 or n > 3: 
-                pass
-            else:
+            level = int(input("Level: "))
+            if level in [1,2,3]: 
                 break
         except ValueError:
             pass
+    return level
 
 def generate_integer(level):
-    while True:
-        try:
-            x = random.randint(1,10)
-            y = random.randint(1,10)
-            return x, y
-        except ValueError:
-            print("EEE")
-            pass
+    if level == 1:
+        x = random.randint(0,9)
+        y = random.randint(0,9)
+    elif level ==2:
+        x = random.randint(10,99)
+        y = random.randint(10,99)
+    else:
+        x = random.randint(100,999)
+        y = random.randint(100,999)
+    return x,y
 
+def simulate_round(x , y):
+    count_tries = 1
+    while count_tries <= 3:
+        try:
+            answer = int(input(f"{x} + {y} = "))
+            if answer == (x + y):
+                return True
+            else:
+                count_tries += 1
+                print("EEE")
+        except:
+            count_tries += 1
+            print("EEE")
+    print(f"{x} + {y} = {x+y}")
+    return False
+
+def simulate_game(level):
+    count_round = 1
+    score = 0
+    while count_round <= 10:
+        x, y = generate_integer(level)
+        response = simulate_round(x, y)
+        if response == True:
+            score += 1
+        count_round += 1
+    return score
 
 if __name__ == "__main__":
     main()
