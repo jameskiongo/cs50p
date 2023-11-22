a834572b46b3a27ef4ec507a47d684978560fd42
james kiongo <jameskiongo@jamess-MacBook-Air.local>
2023-11-22 11:54:38 +0300

    unit test

--- unit-tests/test_hello.py
+++ unit-tests/test_hello.py
@@ -1,4 +1,8 @@
 from hello import hello
 
-def test_hello():
-    hello("David") == "hello, David"
+def test_default():
+    assert hello("David") == "Hello, David"
+    
+def test_argument():
+    
+    assert hello() == "Hello, world"
