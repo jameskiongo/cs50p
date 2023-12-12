333ff4ccb07ae9de52c16aa66037a42226d1f4bc
james kiongo <jameskiongo@jamess-MacBook-Air.local>
2023-12-12 14:28:55 +0300

    started scourgify assnmnt

---
+++ fileio/scourgify/scourgify.py
@@ -0,0 +1,14 @@
+import sys
+
+def main():
+    check_arg
+def check_arg():
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    if len(sys.argv) > 3:
+        sys.exit("Too many command-line arguments")
+    if ".csv" not in sys.argv[1]:
+        sys.exit(f"Could not read {sys.argv[1]}")
+        
+if __name__ == "__main__":
+    main()
